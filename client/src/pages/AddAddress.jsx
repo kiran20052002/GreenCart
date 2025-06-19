89fbbdf7ad5d665bbbf7a0c947cfe72f31269bf0
kiran20052002 <avanchakirankumar2005@gmail.com>
2025-06-19 16:13:00 +0530

    add to cart page

---
+++ client/src/pages/AddAddress.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Input Field Component
+const InputField = () => {
+    <input type="text"/>
+}
+const AddAddress = () => {
+  return (
+    <div >
+        address
+    </div>
+  )
+}
+
+export default AddAddress
