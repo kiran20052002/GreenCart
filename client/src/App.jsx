e071822277596b181c1e4e279a82bb6b83bbf90f
kiran20052002 <avanchakirankumar2005@gmail.com>
2025-06-20 09:48:02 +0530

    added Add Product file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,15 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/seller/SellerLayout";
+import ProductList from "./pages/seller/ProductList";
+import Orders from "./pages/seller/Orders";
+import AddProduct from "./pages/seller/AddProduct";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const { showUserLogin } = useAppContext();
+  const { showUserLogin, isSeller } = useAppContext();
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
@@ -32,6 +37,13 @@ const App = () => {
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/add-address" element={<AddAddress/>}/>
           <Route path="/my-orders" element={<MyOrders/>}/>
+          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+            <Route index element={isSeller ? <AddProduct/> : null}/>
+            <Route path="product-list" element={<ProductList/>}/>
+            <Route path="orders" element={<Orders/>}/>
+
+          </Route>
+
           
         </Routes>
 
