fee690fa4de884fa2f6fcb5a26c6626393a2f33a
kiran20052002 <avanchakirankumar2005@gmail.com>
2025-12-06 09:09:48 +0530

    updated Orders.jsx file

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -49,12 +49,12 @@ const Orders = () => {
 
                     <div className="text-sm md:text-base text-black/60">
                         <p className='text-black/80'>
-                        {order.address.firstName} {order.address.lastName}</p>
+                        {order.address?.firstName} {order.address?.lastName}</p>
 
-                        <p>{order.address.street}, {order.address.city}</p>
-                        <p> {order.address.state}, {order.address.zipcode}, {order.address.country}</p>
+                        <p>{order.address?.street}, {order.address?.city}</p>
+                        <p> {order.address?.state}, {order.address?.zipcode}, {order.address?.country}</p>
                         <p></p>
-                        <p>{order.address.phone}</p>
+                        <p>{order.address?.phone}</p>
                     </div>
 
                     <p className="font-medium text-lg my-auto">
